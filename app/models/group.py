0c9cbdb8f2266b7132b2cc5a2d5e76eb30e917a1
BionicHawk <angel.manzo.rosas@gmail.com>
2024-04-25 08:43:45 -0600

    Defining channels

--- app/models/group.py
+++ app/models/group.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel
 from datetime import datetime
-from .user_participant import UserParticipant
-from .role_definition import RoleDefinition
+from .user_participant import UserParticipant, parse_participant_from_mongo_dict
+from .role_definition import RoleDefinition, parse_role_from_mongo_dict
 from .admin import Admin
 from .normal_participant import NormalParticipant
+
 
 class Group(BaseModel):
     id: str | None
@@ -13,4 +14,17 @@ class Group(BaseModel):
         Admin(),
         NormalParticipant()
     ]
-    creationDate: datetime
+    creationDate: datetime = datetime.now()
+
+
+def parse_group_from_mongo_dict(group: dict) -> Group:
+    group_obj = Group(
+        id=str(group["_id"]),
+        name=group["name"],
+        members=[parse_participant_from_mongo_dict(
+            user) for user in group["members"]],
+        roles=[parse_role_from_mongo_dict(role) for role in group["roles"]],
+        creationDate=datetime.fromisoformat(group["creationDate"])
+    )
+
+    return group_obj
