62427b04672a84e9682bce85c1bae4b2c97f7004
BionicHawk <angel.manzo.rosas@gmail.com>
2024-04-24 18:38:55 -0600

    Indenting xd

--- app/api/routes/users_route.py
+++ app/api/routes/users_route.py
@@ -3,30 +3,30 @@ from starlette.status import *
 from ...models.user import User, parse_user_from_mongo_dict
 from ...models.dto.user_register import UserRegister, build_user_from_register
 from ...models.dto.user_login import UserLogin, build_login_dict
-from ...models.dto.user_at_client import UserClient, parse_user_from_mongo_dict
+from ...models.dto.user_at_client import parse_user_from_mongo_dict
 from ...db.db_context import db
 from ...auth.token_service import get_user_token, authenticate
 
 users_route = APIRouter()
 
-# Register a new user
+
 @users_route.post('/register')
 def register(userRegister: UserRegister):
     matching_user = db.users.find_one(
-        {"$or":[{"email": userRegister.email}, {"username": userRegister.username}]})
-    
+        {"$or": [{"email": userRegister.email}, {"username": userRegister.username}]})
+
     if matching_user != None:
         return Response(status_code=HTTP_400_BAD_REQUEST, content="USER_EXISTS")
-    
+
     db.users.insert_one(build_user_from_register(userRegister))
     return Response(status_code=HTTP_204_NO_CONTENT)
 
-# Login and get token
+
 @users_route.post('/login')
 def login(userLogin: UserLogin):
     login_dict = build_login_dict(userLogin)
     matched_user = db.users.find_one(login_dict)
-    
+
     if matched_user == None:
         return Response(status_code=HTTP_404_NOT_FOUND)
 
@@ -35,6 +35,8 @@ def login(userLogin: UserLogin):
     return token
 
 # Authenticate from token
+
+
 @users_route.get('/authenticate')
 def auth(token: str = Header(default=None)):
     if token == None:
