5e9c6eff6cb26cd4139fa6296af61891298a8686
BionicHawk <angel.manzo.rosas@gmail.com>
2024-05-21 13:26:13 -0600

    Authenticate tests are done! also I had to make the token env variables so the tests work everywhere

--- app/tests/users_test.py
+++ app/tests/users_test.py
@@ -4,10 +4,18 @@ from fastapi.testclient import TestClient
 from ..models.dto.user_register import UserRegister
 from ..models.dto.user_login import UserLogin
 from ..main import app
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+VALID_TOKEN = os.getenv("VALID_TOKEN")
+INVALID_TOKEN_EXPIRED = os.getenv("INVALID_TOKEN_EXPIRED")
 
 client = TestClient(app)
 
 # MARK:Register tests
+
 
 def test_register_r1():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
@@ -24,6 +32,7 @@ def test_register_r1():
 
     assert res.status_code == 400 and message == 'SOME_EMPTY_FIELDS'
 
+
 def test_register_r2():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
     register = UserRegister(
@@ -38,7 +47,8 @@ def test_register_r2():
     message = res.json()['message']
 
     assert res.status_code == 400 and message == 'NOT_VALID_EMAIL'
-    
+
+
 def test_register_r3():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
     register = UserRegister(
@@ -53,6 +63,7 @@ def test_register_r3():
     message = res.json()['message']
 
     assert res.status_code == 400 and message == 'NOT_VALID_PASSWORD'
+
 
 def test_register_r4():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
@@ -73,6 +84,7 @@ def test_register_r4():
 
     assert res2.status_code == 400 and message == 'USER_EXISTS'
 
+
 def test_register_r5():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
     register = UserRegister(
@@ -91,6 +103,7 @@ def test_register_r5():
 
 # MARK: Login Tests
 
+
 def test_login_r1():
     login = UserLogin(
         email="angel.manzorsuanl.edu.mx",
@@ -101,7 +114,8 @@ def test_login_r1():
     message = res.json()['message']
 
     assert res.status_code == 400 and message == 'NOT_VALID_EMAIL'
-    
+
+
 def test_login_r2():
     login = UserLogin(
         email="angel.manzors@uanl.edu.mx",
@@ -112,8 +126,9 @@ def test_login_r2():
     message = res.json()['message']
 
     assert res.status_code == 400 and message == 'NOT_VALID_PASSWORD'
-    
-def test_login_r3(): 
+
+
+def test_login_r3():
     login = UserLogin(
         email="thisuserdoesnotexist@gmail.com",
         password="Str0ng3stP455!"
@@ -122,7 +137,8 @@ def test_login_r3():
     res = client.post('/users/login', json=login)
 
     assert res.status_code == 404
-    
+
+
 def test_login_r4():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
     register = UserRegister(
@@ -145,4 +161,30 @@ def test_login_r4():
     res_login = client.post("/users/login", json=login)
     assert res_login.status_code == 200
 
-    
+# MARK: Authenticate tests
+
+
+def test_authenticate_r1():
+    res = client.get("/users/authenticate")
+    message = res.json()["message"]
+    assert res.status_code == 400 and message == 'NOT_GIVEN_TOKEN'
+
+
+def test_authenticate_r2():
+    res = client.get("/users/authenticate",
+                     headers={
+                         "token": INVALID_TOKEN_EXPIRED
+                     })
+
+    message = res.json()["message"]
+
+    assert res.status_code == 400 and message == 'NOT_VALID_TOKEN'
+
+
+def test_authenticate_r3():
+    res = client.get("/users/authenticate",
+                     headers={
+                         "token": VALID_TOKEN
+                     }
+                     )
+    assert res.status_code == 200
