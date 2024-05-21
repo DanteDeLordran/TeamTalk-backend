ca8d1ebaaff7200a447acf0341a0add8b179ed02
BionicHawk <angel.manzo.rosas@gmail.com>
2024-05-21 12:54:03 -0600

    Login tests are done, also I fixed a typo of one of the API responses (SOME_EMPTY_FIELDS) in register

--- app/tests/users_test.py
+++ app/tests/users_test.py
@@ -2,9 +2,8 @@ import pytest
 import uuid
 from fastapi.testclient import TestClient
 from ..models.dto.user_register import UserRegister
+from ..models.dto.user_login import UserLogin
 from ..main import app
-from random import randint
-import json
 
 client = TestClient(app)
 
@@ -21,7 +20,9 @@ def test_register_r1():
     ).model_dump()
 
     res = client.post('/users/register', json=register)
-    assert res.status_code == 400
+    message = res.json()['message']
+
+    assert res.status_code == 400 and message == 'SOME_EMPTY_FIELDS'
 
 def test_register_r2():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
@@ -34,7 +35,9 @@ def test_register_r2():
     ).model_dump()
 
     res = client.post('/users/register', json=register)
-    assert res.status_code == 400
+    message = res.json()['message']
+
+    assert res.status_code == 400 and message == 'NOT_VALID_EMAIL'
     
 def test_register_r3():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
@@ -47,7 +50,9 @@ def test_register_r3():
     ).model_dump()
 
     res = client.post('/users/register', json=register)
-    assert res.status_code == 400
+    message = res.json()['message']
+
+    assert res.status_code == 400 and message == 'NOT_VALID_PASSWORD'
 
 def test_register_r4():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
@@ -64,7 +69,9 @@ def test_register_r4():
     res1 = client.post("/users/register", json=register)
     res2 = client.post("/users/register", json=register)
 
-    assert res2.status_code == 400
+    message = res2.json()['message']
+
+    assert res2.status_code == 400 and message == 'USER_EXISTS'
 
 def test_register_r5():
     uuid_str = str(uuid.uuid4()).replace('-', '.')
@@ -82,3 +89,60 @@ def test_register_r5():
 
     assert res.status_code == 201
 
+# MARK: Login Tests
+
+def test_login_r1():
+    login = UserLogin(
+        email="angel.manzorsuanl.edu.mx",
+        password="Str0ng3stP455!"
+    ).model_dump()
+
+    res = client.post('/users/login', json=login)
+    message = res.json()['message']
+
+    assert res.status_code == 400 and message == 'NOT_VALID_EMAIL'
+    
+def test_login_r2():
+    login = UserLogin(
+        email="angel.manzors@uanl.edu.mx",
+        password="WeakPassword"
+    ).model_dump()
+
+    res = client.post('/users/login', json=login)
+    message = res.json()['message']
+
+    assert res.status_code == 400 and message == 'NOT_VALID_PASSWORD'
+    
+def test_login_r3(): 
+    login = UserLogin(
+        email="thisuserdoesnotexist@gmail.com",
+        password="Str0ng3stP455!"
+    ).model_dump()
+
+    res = client.post('/users/login', json=login)
+
+    assert res.status_code == 404
+    
+def test_login_r4():
+    uuid_str = str(uuid.uuid4()).replace('-', '.')
+    register = UserRegister(
+        name=f"Angel",
+        lastname="Manzo",
+        username=f"AngelManzo{uuid_str}",
+        email=f"angel.manzors{uuid_str}@uanl.edu.mx",
+        password="Str0ng3rP455!"
+    ).model_dump()
+
+    print(register)
+
+    res_register = client.post("/users/register", json=register)
+
+    login = UserLogin(
+        email=register["email"],
+        password=register["password"]
+    ).model_dump()
+
+    res_login = client.post("/users/login", json=login)
+    assert res_login.status_code == 200
+
+    
